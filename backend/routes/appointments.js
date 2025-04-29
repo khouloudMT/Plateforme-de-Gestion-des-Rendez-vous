63a7282e61c6628ff1de805a10494935a391da3f
Fatmarb <fatmarabai18@gmail.com>
2025-04-29 19:55:38 +0100

    update

--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -56,7 +56,8 @@ router.post('/', [
 
         // Populate professional details for email
         const savedAppointment = await Appointment.findById(appointment._id)
-            .populate('professional', 'name email');
+            .populate('professional', 'name email')
+            .populate('client', 'name email');
 
         // Send confirmation email
         const client = await User.findById(req.user.id);
@@ -66,8 +67,14 @@ router.post('/', [
             subject: 'Appointment Confirmation',
             text: `Your appointment with ${savedAppointment.professional.name} on ${date} at ${time} has been booked.`
         };
+        const emailData2 = {
+            to: professionalUser.email,
+            subject: 'Appointment booked',
+            text: `Your appointment with ${savedAppointment.client.name} on ${date} at ${time} has been booked.`
+        };
 
         await sendEmail(emailData);
+        await sendEmail(emailData2);
 
         res.json(appointment);
     } catch (err) {
@@ -121,8 +128,6 @@ router.get('/my-appointments',
             return res.status(401).json({ msg: 'Not authorized' });
         }
 
-        await updateOutdatedAppointments(appointments);
-
         res.json(appointments);
         
     } catch (err) {
@@ -131,20 +136,7 @@ router.get('/my-appointments',
     }
 });
 
-const updateOutdatedAppointments = async (appointments) => {
-    const now = new Date();
-  
-    for (let appt of appointments) {
-      const appointmentDateTime = new Date(`${appt.date}T${appt.time}`);
-      const limitTime = new Date(appointmentDateTime.getTime() + 30 * 60 * 1000);
-//   && appt.status === 'confirmed'
-      if (now > limitTime ) {
-        appt.status = 'completed';
-        appt.completedAt = now;
-        await appt.save();
-      }
-    }
-  };
+
 
 // @route   GET api/appointments/:id
 // @desc    Get appointment by ID
@@ -205,7 +197,7 @@ router.put('/:id',
             return res.status(401).json({ msg: 'Not authorized' });
           }
     
-        
+    
     
           // Time conflict check (optional)
           if (date || time) {
@@ -216,7 +208,7 @@ router.put('/:id',
               professional: appointment.professional,
               date: checkDate,
               _id: { $ne: appointment._id },
-              $or: [{ time: { $lt: checkTime } }]
+              time: checkTime,
             });
     
             if (conflict) {
@@ -256,8 +248,15 @@ router.put('/:id',
             subject: 'Appointment Updated',
             text: `Your appointment with ${populatedAppointment.professional.name} has been updated. New details: ${appointment.date} at ${appointment.time}. Status: ${appointment.status}`
         };
+        const emailData2 = {
+            to: populatedAppointment.professional.email,
+            subject: 'Appointment Updated',
+            text: `Your appointment with ${populatedAppointment.client.name} has been updated. New details: ${appointment.date} at ${appointment.time}. Status: ${appointment.status}`
+        };
 
         await sendEmail(emailData);
+        await sendEmail(emailData2);
+
 
         res.json(appointment);
     } catch (err) {
@@ -298,9 +297,15 @@ router.delete('/:id', protect,
             subject: 'Appointment Cancelled',
             text: `Your appointment with ${populatedAppointment.professional.name} on ${appointment.date} has been cancelled.`
         };
+        const emailData2 = {
+            to: populatedAppointment.professional.email,
+            subject: 'Appointment Cancelled',
+            text: `Your appointment with ${populatedAppointment.client.name} on ${appointment.date} has been cancelled.`
+        };
 
         await await Appointment.findByIdAndDelete(req.params.id);
         await sendEmail(emailData);
+        await sendEmail(emailData2);
 
         res.json({ msg: 'Appointment removed' });
     } catch (err) {
